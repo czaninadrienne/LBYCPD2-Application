6ab5a279ab291bbc2f34424aa0f67bfde7488ae5
Czanina Tayamen <czanina_tayamen@dlsu.edu.ph>
2021-05-25 12:26:05 +0800

    xml, startup

--- app/src/main/java/com/app/Kawani/StartUp.java
+++ app/src/main/java/com/app/Kawani/StartUp.java
@@ -3,26 +3,29 @@ package com.app.Kawani;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
-
-import android.widget.Button;
-import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    public Button button;
+public class StartUp extends AppCompatActivity {
+
+    int view = R.layout.activity_startup;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        button = (Button)findViewById(R.id.btn);
+        setContentView(view);
+        button = findViewById(R.id.text);
+        button.setText("k");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,ReadQR.class);
+                Intent intent = new Intent(StartUp.this,MainActivity.class);
                 startActivity(intent);
             }
         });
